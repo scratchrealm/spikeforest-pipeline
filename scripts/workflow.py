f1e11fb6fc8279be5c54cdff8c4274236873453a
Jeremy Magland <jeremy.magland@gmail.com>
2022-02-04 10:18:03 -0500

    clean up workflow

--- scripts/workflow.py
+++ scripts/workflow.py
@@ -23,31 +23,38 @@ class Workflow:
 @click.command()
 @click.argument('config_file')
 def main(config_file: str):
+    # Load configuration file
     with open(config_file, 'r') as f:
         config = yaml.safe_load(f)
     config_name = config['name']
     config_sorters = config['sorters']
     config_studies = config['studies']
 
+    # Load spikeforest study sets data
     sf_study_sets_uri = 'sha1://f728d5bf1118a8c6e2dfee7c99efb0256246d1d3/studysets.json'
     sf_study_sets = kc.load_json(sf_study_sets_uri)
     assert sf_study_sets is not None, f'Unable to load sf study sets: {sf_study_sets_uri}'
 
+    # Initialize the workflow
     workflow = Workflow()
-    
-    # study1 = kc.load_json('sha1://7b85423dc46f34ebd7b12559d6d2131de1eb7473/paired_kampff.json')
-    # recordings = study1['recordings'][:1]
 
-    for config_study in config_studies:
+    for config_study in config_studies: # for each study
+        # Get the sorters to be run for this study
         config_sorters = [[s for s in config_sorters if s['name'] == sorter_name][0] for sorter_name in config_study['sorter_names']]
-        for recording_name in config_study['recording_names']:
+        for recording_name in config_study['recording_names']: # for each recording
+            # load the recording dict from the spikeforest study sets
             recording = _get_spikeforest_recording(sf_study_sets, config_study['study_set_name'], config_study['study_name'], recording_name)
+            # prepare recording.nwb
             recording_nwb_uri = _prepare_recording_nwb(workflow, recording)
+            # prepare sorting_true.npz
             sorting_true_npz_uri = _prepare_sorting_true_npz(workflow, recording)
-            for sorter in config_sorters:
+            for sorter in config_sorters: # for each sorter
+                # do the spike sorting for the given sorter
                 sorting_npz_uri = _sorting(workflow, recording, recording_nwb_uri, sorter)
+                # compare with truth
                 comparison_uri = _compare_with_truth(workflow, recording, sorter, sorting_npz_uri, sorting_true_npz_uri)
                 if sorting_npz_uri is not None and comparison_uri is not None and kc.load_file(comparison_uri, local_only=True) is not None:
+                    # if everything has completed for this recording/sorter, add the result to the workflow
                     workflow.add_result({
                         'recording': recording,
                         'sorter': sorter,
@@ -56,13 +63,21 @@ def main(config_file: str):
                         'sorting_npz_uri': sorting_npz_uri,
                         'comparison_with_truth': kc.load_json(comparison_uri)
                     })
+    # Set the list of jobs as a kachery mutable
     kc.set({'type': 'spikeforest-workflow-jobs', 'name': config_name}, [job.to_dict() for job in workflow.jobs])
+    # Set the list of results as a kachery mutable
     kc.set({'type': 'spikeforest-workflow-results', 'name': config_name}, workflow.results)
     print('-----------------------------')
+    # Print the jobs
+    print('JOBS:')
     for job in workflow.jobs:
-        print(job.type)
+        print(f'{job.type}: {job.label}')
     print('-----------------------------')
-    print(workflow.results)
+    # Print the results
+    print('RESULTS:')
+    for result in workflow.results:
+        print(f'{result["sorter"]["name"]} {result["recording"]["studyName"]}/{result["recording"]["name"]}')
+    print('-----------------------------')
 
 def _prepare_recording_nwb(workflow: Workflow, recording: dict):
     recording_label = f'{recording["studyName"]}/{recording["name"]}'
