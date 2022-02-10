477ca3adfa0a03d9d0f7c558afc37cf6b171be49
Jeremy Magland <jeremy.magland@gmail.com>
2022-02-10 16:01:41 -0500

    kilosort3

--- scripts/sorting.py
+++ scripts/sorting.py
@@ -24,6 +24,8 @@ def _run_sorting_job(algorithm: str, recording_nwb_uri: str, sorting_params: dic
             subpath = 'spykingcircus'
         elif algorithm == 'tridesclous':
             subpath = 'tridesclous'
+        elif algorithm == 'kilosort3':
+            subpath = 'kilosort3'
         else:
             raise Exception(f'Unexpected algorithm: {algorithm}')
 
